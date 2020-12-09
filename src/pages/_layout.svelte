b1675a402c7ce16b21764c4fefd9f89fc79fb471
Johnny Huynh <info@johnnyhuy.com>
2020-12-09 15:59:46 +1100

    fix: Move static files to assets

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,13 +1,2 @@
 <!-- routify:options preload="proximity" -->
-<head>
-    <meta charset="utf-8" />
-    <link rel="apple-touch-icon" href="/_dist_/favicon/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/_dist_/favicon/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/_dist_/favicon/favicon-16x16.png" />
-    <link rel="manifest" href="/manifest.json" />
-    <link rel="mask-icon" href="/_dist_/favicon/safari-pinned-tab.svg" color="#ff8d00" />
-    <meta name="msapplication-TileColor" content="#ffffff" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Welcome to John's portfolio blog or whatever 🤓" />
-  </head>
 <slot />
