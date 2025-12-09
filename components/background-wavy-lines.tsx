6acf123a1b29ba58c90ed0051deea9a1fab043d8
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2025-12-10 01:10:43 +1100

    fix: update SVG viewBox dimensions and preserveAspectRatio attribute for BackgroundWavyLines component

--- components/background-wavy-lines.tsx
+++ components/background-wavy-lines.tsx
@@ -28,7 +28,8 @@ export const BackgroundWavyLines: FC<BackgroundWavyLinesProps> = () => {
       <svg
         width="100%"
         height="100%"
-        viewBox="0 0 595 232"
+        viewBox="0 0 712 232"
+        preserveAspectRatio="none"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         className="h-full w-full"
